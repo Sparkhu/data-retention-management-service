f1be900065cccd0b99b5d7708279adbf6d1131ff
AnYoungMin <youngmin.develop@gmail.com>
2022-05-16 02:43:48 +0900

    Initialize project

---
+++ src/test/java/net/icnslab/sparkhu/dataretentionmanagementservice/DataRetentionManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.icnslab.sparkhu.dataretentionmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataRetentionManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
